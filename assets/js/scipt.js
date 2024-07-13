8af469b1877ae8ead78464885d62f32267f63c6c
seekode <nicolas@seekode.dev>
2024-07-14 00:46:45 +0200

    End of design for index.html

--- assets/js/scipt.js
+++ assets/js/scipt.js
@@ -1,6 +1,11 @@
 const nav = document.getElementById("nav");
 const menuButton = document.getElementById("menu-button");
 
+nav.addEventListener("click", () => {
+	nav.classList.add("open");
+	menuButton.classList.add("open");
+});
+
 menuButton.addEventListener("click", () => {
 	nav.classList.toggle("open");
 	menuButton.classList.toggle("open");
